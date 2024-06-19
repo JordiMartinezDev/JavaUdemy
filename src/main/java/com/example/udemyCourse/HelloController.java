39446fc6e8377749f209f1ffffb0a4c2683edfdd
JordiMartinezDev <jordimartinezdev@gmail.com>
2024-06-19 08:23:07 +0000

    added Requestmapping on the class

--- src/main/java/com/example/udemyCourse/HelloController.java
+++ src/main/java/com/example/udemyCourse/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.udemyCourse;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/myapp")
 public class HelloController {
     
     @GetMapping("/")
     public String index(){
         return "Hello Worldss ";
+    }
+    @GetMapping("/test")
+    public String test(){
+        return "test";
     }
 }
